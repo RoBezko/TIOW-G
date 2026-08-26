1ef3e59c743b3ef58f3bc1d99db7a807425e5084
Roko <167866280+RoBezko@users.noreply.github.com>
2026-08-26 18:39:10 +0300

    VisualStuff

--- scripts/Menu/main_menu.gd
+++ scripts/Menu/main_menu.gd
@@ -2,8 +2,14 @@ extends Control
 
 @export var main_scene: MainScene
 
+@onready var back: Sprite2D = $Back
+
 func _on_play_pressed() -> void:
 	if not Global.startAnimFinished:
 		main_scene.start_intro()
 	else:
 		main_scene.start_game()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		pass
